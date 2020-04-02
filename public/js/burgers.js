841b1fe060049401c17a75b647c123083d1c07da
Java K <karna.cutolo@gmail.com>
2020-04-02 17:31:01 -0400

    heroku issue

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -23,27 +23,7 @@ $(function() {
       );
     });
   
-    // $(".create-form").on("submit", function(event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
-  
-    //   var newCat = {
-    //     name: $("#ca").val().trim(),
-    //     sleepy: $("[name=sleepy]:checked").val().trim()
-    //   };
-  
-    //   // Send the POST request.
-    //   $.ajax("/api/cats", {
-    //     type: "POST",
-    //     data: newCat
-    //   }).then(
-    //     function() {
-    //       console.log("created new cat");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+    
   
     $(".delete-burger").on("click", function(event) {
       var id = $(this).data("id");
